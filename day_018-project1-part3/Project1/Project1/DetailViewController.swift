6dbc6d307728ac59d1f954ec43dcd94888674d4e
JLCS <salvador.rhenz@gmail.com>
2022-03-30 15:58:09 +0800

    Day18 completed - project1

---
+++ day_018-project1-part3/Project1/Project1/DetailViewController.swift
@@ -0,0 +1,61 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by JLCS on 3/29/22.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    // MARK: - IBOutlets
+    @IBOutlet weak var imageView: UIImageView!
+    
+    // MARK: - Stored Properties
+    var selectedImageName: String?
+    var selectedImageIndex: Int?
+    var totalNumberOfImages: Int?
+    
+    // MARK: - View Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+    // MARK: - Private Methods
+    private func setupViews() {
+        navigationItem.largeTitleDisplayMode = .never
+        
+        if let imageIndex = self.selectedImageIndex,
+           let imageCount = self.totalNumberOfImages {
+            self.title = "Picture \(imageIndex+1) of \(imageCount)"
+        } else {
+            setTitleToImageName()
+        }
+        
+        setImageView()
+    }
+    
+    private func setTitleToImageName() {
+        if let imageName = selectedImageName {
+            self.title = imageName
+        }
+    }
+    
+    private func setImageView() {
+        if let imageName = selectedImageName {
+            self.imageView.image = UIImage(named: imageName)
+        }
+    }
+}
---
+++ day_018-project1-part3/README.md
@@ -0,0 +1,12 @@
+# Day 18: Project 1, part three
+
+## Notes
+
+## Challenges:
+- Use Interface Builder to select the text label inside your table view cell and adjust its font size to something larger – experiment and see what looks good.
+- In your main table view, show the image names in sorted order, so “nssl0033.jpg” comes before “nssl0034.jpg”.
+- Rather than show image names in the detail title bar, show “Picture X of Y”, where Y is the total number of images and X is the selected picture’s position in the array. Make sure you count from 1 rather than 0.
+
+## Screenshots:
+![App-Screenshot](documentation/1.gif)
+![App-Screenshot](documentation/1.png)
